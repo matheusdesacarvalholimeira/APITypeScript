df451907d61efdd4d5b0a3d8bfcc75100c07399b
Matheus <118131034+matheusdesacarvalholimeira@users.noreply.github.com>
2024-05-13 01:24:34 -0300

    http status code

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { StatusCodes } from "http-status-codes"
 
 const router = Router();
 
@@ -13,5 +14,31 @@ router.get("/enviar", (req, res)=>{
     return res.send("Motrando mais no body");
 
 });
+
+router.get("/retornarbody", (req, res)=>{
+    return res.json(req.body);
+});
+
+router.get("/pegandoURl/:id", (req, res)=>{
+    //dessa forma e quando e obrigatorio a passagem desse parametro
+    return console.log(req.params.id);
+})
+
+router.get("/passandoURLDiferente",(req, res)=>{
+    //?id=ola
+    //deve ser colocado dessa forma na URl
+    return console.log(req.query.id)
+
+})
+
+//biblioteca de status code foi adicionada e serve para retornar o status de maneira meis correta
+router.get("/statusCode", (req, res)=>{
+    return res.status(401).json(req.body);
+})
+
+//biblioteca de status code foi adicionada e serve para retornar o status de maneira meis correta
+router.get("/statusCode", (req, res)=>{
+    return res.status(StatusCodes.ACCEPTED).json(req.body);
+})
 
 export {router};
